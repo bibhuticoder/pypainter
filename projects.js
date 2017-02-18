8aa16da7e690eca3745f2e9b8ce4a912960cf2ba
Bibhuti Poudyal <bibhutipd@gmail.com>
2017-02-18 22:50:16 +0545

    pypainter added

---
+++ projects.js
@@ -0,0 +1,197 @@
+projects = [
+
+    {
+        name: 'RealtimeWhiteboard',
+        description: 'Collaborative whiteboard in nodejs with socket.io. Allows you to chat and share your illustrations with your friends.',
+        code: 'https://github.com/bibhuticoder/realtimeBoard',
+        demo: 'https://realtimeboard.herokuapp.com'
+
+    },
+    {
+        name: 'TheNewsBroker',
+        description: 'An online news platform where you can read varieties of news at one place.',
+        code:  null,
+        demo: 'http://thenewsbroker.tk'
+
+    },
+    {
+        name: 'DesignPad',
+        description: 'A web app to design UI, diagrams, flowchart and many more. Backed by fabric js.',
+        code: 'https://github.com/bibhuticoder/designpad',
+        demo: 'https://bibhuticoder.github.io/designpad'
+
+    },
+     {
+        name: 'Loadshedding',
+        description: 'A web app and an API for latest Nepal loadshedding updates. ',
+        code: 'https://github.com/bibhuticoder/loadshedding',
+        demo: 'http://meroloadsheddingapp.herokuapp.com/'
+
+    },
+    {
+        name: 'Typeracer',
+        description: 'A web app to make typing fun and competative. Both multiplayer and single player(AI).',
+        code: 'https://github.com/bibhuticoder/typeracer',
+        demo: 'http://merotyperacer.herokuapp.com/'
+
+    },   
+    {
+        name: 'ToDos',
+        description: 'A simple todos app in nodejs with mongoDb. Clean and simpe UI.',
+        code: 'https://github.com/bibhuticoder/todos',
+        demo: 'https://merotodos.herokuapp.com'
+
+    },
+
+    {
+        name: 'Multiplayer Snake',
+        description: 'Multiplayer snake in JavaScript with node and socket.io',
+        code: 'https://github.com/bibhuticoder/multiplayersnake',
+        demo: 'https://realtimesnake.herokuapp.com'
+
+    },
+
+    {
+        name: 'Tick Tak Toe',
+        description: 'Double player tick-tak-toe game coded in javascript with node as backend. Socket.io used for data communication.',
+        code: 'https://github.com/bibhuticoder/ticktaktoe',
+        demo: 'https://ticktaktoert.herokuapp.com'
+
+    },
+
+    {
+        name: 'Stick-hero',
+        description: 'Popular android game "Stick hero" clone in plain JavaScript.',
+        code: 'https://github.com/bibhuticoder/stick-hero',
+        demo: 'https://bibhuticoder.github.io/stick-hero'
+
+    },
+
+    {
+        name: 'PrefixPostfix',
+        description: 'Convert mathematical infix expressions to prefix/postfix and solve it. A visual representation.',
+        code: 'https://github.com/bibhuticoder/prefixpostfix',
+        demo: 'https://bibhuticoder.github.io/prefixpostfix'
+
+    },
+
+    {
+        name: 'TowerOfHanoi',
+        description: 'Give the number "n" and it solves the Tower of Hanoi for you in an illustrative way.',
+        code: 'https://github.com/bibhuticoder/towerofhanoi',
+        demo: 'https://bibhuticoder.github.io/towerofhanoi'
+
+    },
+
+    {
+        name: 'Bubbletrouble',
+        description: 'The clone of "Bubble Trouble" game in JavaScript.',
+        code: 'https://github.com/bibhuticoder/bubbletrouble',
+        demo: 'https://bibhuticoder.github.io/bubbletrouble'
+
+    },
+
+    {
+        name: 'Stopwatch',
+        description: 'A simple and memory efficient stopwatch in Javascript with simple and beautiful UI',
+        code: 'https://github.com/bibhuticoder/stopwatch',
+        demo: 'https://bibhuticoder.github.io/stopwatch'
+
+    },
+
+    {
+        name: 'Calculator',
+        description: 'Calculator app in Javascript with clean and beautiful UI',
+        code: 'https://github.com/bibhuticoder/calculator',
+        demo: 'https://bibhuticoder.github.io/calculator'
+
+    },
+
+    {
+        name: 'CircleEscape',
+        description: 'Flappy bird like game where you have to move escaping the circles to earn points.',
+        code: 'https://github.com/bibhuticoder/circleescape',
+        demo: 'https://bibhuticoder.github.io/circleescape'
+
+    },
+
+    {
+        name: 'Notepad',
+        description: 'A simple online notepad app in plain JavaScript.',
+        code: 'https://github.com/bibhuticoder/notepad',
+        demo: 'https://bibhuticoder.github.io/notepad'
+
+    },
+
+    {
+        name: 'MoleculeJs',
+        description: 'A canvas based JavaScript library to add molecule-bonding effect to your webpage. My first attemp to make a javascript library.',
+        code: 'https://github.com/bibhuticoder/moleculejs',
+        demo: 'https://bibhuticoder.github.io/moleculejs'
+
+    },
+
+    {
+        name: 'Snake',
+        description: 'A canvas based simple snake game in plain JavaScript.',
+        code: 'https://github.com/bibhuticoder/snake',
+        demo: 'https://bibhuticoder.github.io/snake'
+
+    },
+
+    {
+        name: 'CalculateJs',
+        description: 'Given a mathematical expression, this javascript library gives the final result.',
+        code: 'https://github.com/bibhuticoder/calculatejs',
+        demo: 'https://bibhuticoder.github.io/calculatejs'
+
+    },
+
+    {
+        name: 'ColorPicker',
+        description: 'A color picker desktop application in C#. Supports to pick and generate new colors.',
+        code: 'https://github.com/bibhuticoder/colorpicker',
+        demo: null
+
+    },
+
+    {
+        name: 'PingPong',
+        description: 'A HTML5 canvas based pingpong game in plain JavaScript.',
+        code: 'https://github.com/bibhuticoder/pingpong',
+        demo: 'https://bibhuticoder.github.io/pingpong'
+
+    },
+
+    {
+        name: 'Queue',
+        description: 'The implementation of Queue Data Structure in JavaScript with illustration.',
+        code: 'https://github.com/bibhuticoder/queue',
+        demo: 'https://bibhuticoder.github.io/queue'
+
+    },
+    
+    {
+        name: 'QtSnake',
+        description: 'Snake game developed in c++ using QT GUI framework. Also, implementation of different snake AI.',
+        code: 'https://github.com/bibhuticoder/QtSnake',
+        demo:null
+
+    },
+    
+    {
+        name: '2 Cars',
+        description: '2 Cars game clone in JavaScript. ',
+        code: 'https://github.com/bibhuticoder/2cars',
+        demo: 'https://bibhuticoder.github.io/2cars/'
+
+    },
+
+    {
+        name: 'pyPainter',
+        description: 'Simply paint and watch the timelapse. Feature to fast and slow the timelapse.',
+        code: 'https://github.com/bibhuticoder/pypainter',
+        demo: 'https://bibhuticoder.github.io/pypainter/'
+
+    }
+];
